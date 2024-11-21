74dc997315ee4acb88ecc38642de0ba93dd158b3
nouramahmoud <Noura.mahmoud.mustafa@gmail.com>
2024-11-21 18:58:38 +0100

    Fix Commands file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,16 @@
 Cypress.Commands.add('handleCookiesAndPopups', () => {
     // Handle cookie notification
-    cy.get('#shopify-pc__banner__btn-accept', { timeout: 100000 })
+  /*  cy.get('#shopify-pc__banner__btn-accept', { timeout: 10000 })
         .should('be.visible')
         .click();
-    cy.pause();
+    cy.pause();*/
+    cy.get('body').then(($body) => {
+        if ($body.find('#shopify-pc__banner__btn-accept').length > 0) {
+            cy.get('#shopify-pc__banner__btn-accept',{ timeout: 100000 }).click();
+        } else {
+            cy.log('No cookie notification, proceeding...');
+        }
+    });
     // Check for Free Matcha Popup and remove it if it exists
     cy.get('body').then(($body) => {
         if ($body.find('#one-click-popup-13553372974531').length > 0) {
